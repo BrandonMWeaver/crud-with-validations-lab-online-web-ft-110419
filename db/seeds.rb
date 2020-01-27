e20dfd8c3c2d557389d1936d3f8569c0fa5a0fcc
Brandon Weaver <a7xncob@gmail.com>
2020-01-27 01:34:48 +0000

    Automatically backed up by Learn

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,1 @@
-Song.create(title: "Test", released: false, release_year: 1990, artist_name: "Test Artist",)
+Song.create(title: "Test", released: false, release_year: 1990, artist_name: "Test Artist", genre: "Test Genre")
