7610325e29b55494b7026a306444ff3715ec9cbb
Brandon Weaver <a7xncob@gmail.com>
2020-01-27 02:00:45 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -15,6 +15,11 @@ class SongsController < ApplicationController
   
   def create
     song = Song.new(song_params(:title, :released, :release_year, :artist_name, :genre))
+    if song.save
+      redirect_to song_path(song)
+    else
+      render "new"
+    end
   end
   
   def edit
