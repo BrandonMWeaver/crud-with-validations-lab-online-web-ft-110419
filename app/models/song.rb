8cc6b22bff853101c87b330ac6aa8427a57685e5
Brandon Weaver <a7xncob@gmail.com>
2020-01-27 01:21:38 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,7 +2,7 @@ class Song < ApplicationRecord
   validates :title, presence: true, uniqueness: true
   validates :released, inclusion: [true, false]
   with_options if: released? do |song|
-    
+    song.validates
   validates :artist_name, presence: true
   
   def released?
