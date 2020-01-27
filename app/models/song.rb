ea65861a47cf6ff0c406d95a31637c1b74a6c8b3
Brandon Weaver <a7xncob@gmail.com>
2020-01-27 01:22:53 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,6 +4,8 @@ class Song < ApplicationRecord
   with_options if: released? do |song|
     song.validates :release_year, presence: true
     song.validates :release_year, numericality {
+      less_than_or_equal_to: Date.
+    }
   validates :artist_name, presence: true
   
   def released?
